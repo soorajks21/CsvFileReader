5f4fa3de029b8875495ae80c53061455247606f1
soorajks21 <soorajks@live.in>
2021-12-09 13:16:09 -0500

    Routes: Routes For Emp & Report
    Routes for fetching employee and report generations

---
+++ routers/csv_router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../config/multer.config');
+
+const { uploadFile, getReport } = require('../controllers/CsvController');
+
+router.post('/api/file/upload', upload.single("file"), uploadFile, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+});
+router.get('/api/report', getReport);
+
+
+
+module.exports = router;
