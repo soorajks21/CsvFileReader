665cf8bef46f4a3d8e2519eb435f751c7c71cabc
soorajks21 <soorajks@live.in>
2021-12-09 12:04:54 -0500

    env & connection: connection string
    environment variables mapped to database and starting port

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const connectDB = async() => {
+    try {
+       
+            
+             await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+                
+            }
+            )
+        }
+      
+    
+    catch (err) {
+        process.exit(1)
+    }
+}
+
+
+const close = () => {
+    return mongoose.disconnect();
+}
+
+module.exports = { connectDB, close };
